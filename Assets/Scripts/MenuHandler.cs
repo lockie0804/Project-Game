902c510ca8f2e9aab69325025acf5def41d70c83
lockie0804 <109713574+lockie0804@users.noreply.github.com>
2022-11-03 18:26:49 +1100

    Updated

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour
+{
+    public void ChangeScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    public void ExitToDesktop()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+}
